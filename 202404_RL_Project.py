10281b55ed2379f80f19c40fd466a9f8cbcdc3f0
dalki <dyd4642@naver.com>
2024-03-12 21:10:22 +0900

    action 데이터프레임 추가 완료

--- 202404_RL_Project.py
+++ 202404_RL_Project.py
@@ -14,15 +14,16 @@ class stablebaselineEnv(gym.Env):
         super(stablebaselineEnv, self).__init__()
         self.action_space = spaces.Discrete(4)  # 0: Long, 1: Short, 2: Close, 3: Hold
         self.observation_space = spaces.Dict({
-            "action": spaces.Discrete(4), # 행동 {0:Long, 1:Short, 2:Close, 3:Hold}
-            "position": spaces.Discrete(3), # 포지션 {0:Long, 1:Short, 2:None}
-            "chart_data": spaces.Box(low=0, high=np.inf, shape=(df.shape[0], df.shape[1]), dtype=np.float32),
-            "current_price": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32), # 현재 가격
-            "pnl": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32), # 미실현 손익
-            "closing_pnl": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32), # 실현 손익
-            "total_pnl": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32), # 누적 손익
-            "total_balance": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32) # 총 자산
-
+            "chart_data": spaces.Box(low=0, high=np.inf, shape=(window_size, df.shape[1]), dtype=np.float32),
+            "position": spaces.Discrete(3),  # 포지션 {0:Long, 1:Short, 2:None}
+            "current_price": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32),  # 현재 가격
+            "avg_price": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32),  # 평균 진입 가격
+            "pnl": spaces.Box(low=-np.inf, high=np.inf, shape=(1,), dtype=np.float32),  # 미실현 손익
+            "total_pnl": spaces.Box(low=-np.inf, high=np.inf, shape=(1,), dtype=np.float32),  # 누적 손익
+            "usdt_balance": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32),  # USDT 잔고
+            "margin": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32),  # 사용 중인 마진
+            "size": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32),  # 포지션 수량
+            "total_balance": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32)  # 총 자산
         })
     
         self.slice_df, self.observation_df, self.train_df = stablebaselineEnv.generate_random_data_slice(df, window_size,test_window_size) # 랜덤 위치로 slice된 차트 데이터 초기화
@@ -42,38 +43,55 @@ class stablebaselineEnv(gym.Env):
         
                 
         """       
-        self.usdt_balance = usdt_balance # 초기 usdt 잔고
-        self.initial_usdt_balance = usdt_balance 
-        self.btc_size = btc_size # 포지션 수량
+        # reset 미포함
+        self.initial_usdt_balance = usdt_balance # 초기 usdt 잔고
         self.min_order = 0.002 # 최소 주문 수량
         self.fee = 0.0005 # 거래 수수료
         self.leverage = leverage # 레버리지
+        
+        # reset 포함
+        self.usdt_balance = usdt_balance # 초기 usdt 잔고
+        self.btc_size = btc_size # 포지션 수량
         self.margin = 0 # 포지션 증거금
         self.position = None # 포지션 {0:Long, 1:Short, 2:None}
-        self.real_action = None # 실제 행동
-
         self.order_price = 0 # 주문 금액
         self.last_size_value = 0 # (평단가 계산에 필요)
         self.current_avg_price = 0 # 현재 평단가
-
         self.pnl = 0 # 미실현 손익
         self.closing_pnl = 0 # 실현 손익
         self.total_pnl = 0 # 누적 손익
         self.total_fee = 0 # 누적 수수료
         self.total_balance = 0 # 총 자산
+        self.action_history = pd.DataFrame(columns=['action'])
+        pass
 
     def reset(self): # 리셋 함수 -> ㅇ
         self.slice_df = stablebaselineEnv.generate_random_data_slice(self.df, self.window_size,self.test_window_size) # reset 하며 새로운 랜덤 차트 데이터 초기화
         self.observation_space = spaces.Dict({
-            "action": spaces.Discrete(4), # 행동 {0:Long, 1:Short, 2:Close, 3:Hold}
-            "position": spaces.Discrete(3), # 포지션 {0:Long, 1:Short, 2:None}
-            "chart_data": spaces.Box(low=0, high=np.inf, shape=(df.shape[0], df.shape[1]), dtype=np.float32),
-            "current_price": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32), # 현재 가격
-            "pnl": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32), # 미실현 손익
-            "closing_pnl": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32), # 실현 손익
-            "total_pnl": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32), # 누적 손익
-            "total_balance": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32) # 총 자산       
-             })
+            "chart_data": spaces.Box(low=0, high=np.inf, shape=(window_size, df.shape[1]), dtype=np.float32),
+            "position": spaces.Discrete(3),  # 포지션 {0:Long, 1:Short, 2:None}
+            "current_price": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32),  # 현재 가격
+            "avg_price": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32),  # 평균 진입 가격
+            "pnl": spaces.Box(low=-np.inf, high=np.inf, shape=(1,), dtype=np.float32),  # 미실현 손익
+            "total_pnl": spaces.Box(low=-np.inf, high=np.inf, shape=(1,), dtype=np.float32),  # 누적 손익
+            "usdt_balance": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32),  # USDT 잔고
+            "margin": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32),  # 사용 중인 마진
+            "size": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32),  # 포지션 수량
+            "total_balance": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32)  # 총 자산
+        })
+        self.usdt_balance = self.initial_usdt_balance # 초기 usdt 잔고
+        self.btc_size = 0 # 포지션 수량
+        self.margin = 0 # 포지션 증거금
+        self.position = None # 포지션 {0:Long, 1:Short, 2:None}
+        self.order_price = 0 # 주문 금액
+        self.last_size_value = 0 # (평단가 계산에 필요)
+        self.current_avg_price = 0 # 현재 평단가
+        self.pnl = 0 # 미실현 손익
+        self.closing_pnl = 0 # 실현 손익
+        self.total_pnl = 0 # 누적 손익
+        self.total_fee = 0 # 누적 수수료
+        self.total_balance = 0 # 총 자산
+        self.action_history = pd.DataFrame(columns=['action'])
         pass
     
 
@@ -152,8 +170,6 @@ class stablebaselineEnv(gym.Env):
 
     def act(self, action):
         action = self.act_check(action)
-        self.real_action = action
-        
         if action == 0 or action == 1:  # Long or Short
             if self.position == action or self.position == 2 or self.position is None:
                 self.open_position(action)
@@ -172,12 +188,16 @@ class stablebaselineEnv(gym.Env):
                 self.pnl = (self.current_avg_price - self.current_price) * self.btc_size * self.leverage
             
             self.total_balance = self.usdt_balance + self.margin
-        '''
-        주문 수량은 일단 항상 최소 주문 금액으로 하겠습니다.
-        최소 수량으로 해도 0.002개 이고 1배율일 경우 증거금 136usdt 정도 들어갑니다.
-        
-        추후 수량이 커질시 미결손실 또한 고려해야함
-        '''
+        return action
+    
+    '''
+    return : self.position, self.acutal_action, self.pnl, self.closing_pnl, self.total_pnl, self.total_balance
+    
+    주문 수량은 일단 항상 최소 주문 금액으로 하겠습니다.
+    최소 수량으로 해도 0.002개 이고 1배율일 경우 증거금 136usdt 정도 들어갑니다.
+    
+    추후 수량이 커질시 미결손실 또한 고려해야함
+    '''
 
 
     def step(self, action):
@@ -185,10 +205,10 @@ class stablebaselineEnv(gym.Env):
             self.slice_df.iloc[self.current_index]['Open'],
             self.slice_df.iloc[self.current_index]['Close']
         ) # 현재 가격을 시가, 종가 사이 랜덤 값으로 결정됨.
-
         
-        self.act(action) # action을 수행함.
-
+        action = self.act(action) # action을 수행함.
+        self.action_history = self.action_history.append({'action': action}, ignore_index=True) # action_history에 action 추가
+        
         reward = None
         obs = None
 
@@ -203,15 +223,13 @@ class stablebaselineEnv(gym.Env):
 
         return obs, reward, done, {}
 
-
-
     def render(self, render_mode=None):
         if render_mode == "human":
             candle = go.Candlestick(open=self.slice_df['Open'],high=self.slice_df['High'],low=self.slice_df['Low'],close=self.slice_df['Close'],increasing_line_color='red',decreasing_line_color='blue',yaxis='y2')
             fig = go.Figure(data=[candle])
 
             # action DataFrame의 각 행에 대해 반복
-            for index, row in self.action.iterrows():
+            for index, row in self.action_history.iterrows():
                 x_position = self.slice_df.index.get_loc(index)  # x축 위치 결정
 
                 if row['action'] == 0:
@@ -238,12 +256,6 @@ class stablebaselineEnv(gym.Env):
             )
 
             fig.show()
-
-
-
-
-
-
 
 
     # 여러가지 보조지표들을 추가하는 함수, 보조지표들을 넣고 싶다면 여기서 수정 진행
